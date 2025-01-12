a67849e28393c88c05729dc4001e43be6836e630
David Vogel <1079016+dvogeldev@users.noreply.github.com>
2025-01-12 16:03:37 -0500

    feat: add lazyvim

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+return {
+  git = {
+    url_format = "git@github.com:%s.git",
+  },
+  colorscheme = "catppuccin-mocha",
+}

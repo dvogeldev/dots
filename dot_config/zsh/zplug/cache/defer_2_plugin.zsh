79540fce2b45de69fe1e84f6064ce60d75d5d36e
David Vogel <dvogelca@proton.me>
2025-01-14 13:03:35 -0500

    upgrade: upgraded my zsh config

---
+++ dot_config/zsh/zplug/cache/defer_2_plugin.zsh
@@ -0,0 +1,2 @@
+__zplug::core::load::as_plugin --repo "zsh-users/zsh-autosuggestions" "/var/home/david/.config/zsh/zplug/repos/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
+__zplug::core::load::as_plugin --repo "zsh-users/zsh-syntax-highlighting" "/var/home/david/.config/zsh/zplug/repos/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh"

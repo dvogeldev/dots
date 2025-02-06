dbb5dc4a7b28d7476912440fad3213be4b93ede1
David Vogel <1079016+dvogeldev@users.noreply.github.com>
2025-02-06 13:40:21 -0500

    Remove nvim configs

--- dot_config/nvim/after/ftplugin/markdown.lua
+++
@@ -1,7 +0,0 @@
-vim.opt_local.spell = true
-vim.opt_local.wrap = true
-vim.opt_local.breakindent = true
-vim.opt_local.textwidth = 100
-
--- Disable autoformat for markdown files
-vim.b.autoformat = false
